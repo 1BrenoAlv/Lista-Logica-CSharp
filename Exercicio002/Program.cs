ae2cea0caafded78dbf2c56845b87c6e05292a29
breno201 <breno.queiroz@websupply.com.br>
2026-02-19 14:59:06 -0300

    feat: outros exercicios avançados feitos

---
+++ Exercicio002/Program.cs
@@ -0,0 +1,50 @@
+﻿using Exercicio002.exercicios;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string modelo = "1 - Jogo de adivinhação\n2 - Manipulação de array\n3 - Calculadora de caixa eletrônico\n4 - Validador de CPF\n5 - Mini banco\n0 - Encerrar";
+
+        Console.WriteLine("Exercicio 2\n");
+        Console.WriteLine("Escolha o Exercicio de sua preferencia e teste:\n");
+        bool laco = true;
+
+        while (laco)
+        {
+            Console.WriteLine(modelo);
+            Console.Write("Digite: ");
+            int escolha = int.Parse(Console.ReadLine());
+
+            switch (escolha)
+            {
+                case 1:
+                    ExercicioAvancado.NumeroAleatorio();
+                    break;
+                case 2:
+                    ExercicioAvancado.ManipulacaoArray();
+                    break;
+                case 3:
+                    ExercicioAvancado.CalculadoraCaixa();
+                    break;
+                case 4:
+                    ExercicioAvancado.ValidadorCPF();
+                    break;
+                case 5:
+                    ExercicioAvancado.MiniBanco();
+                    break;
+                case 0:
+                    Console.WriteLine("Programa Encerrado!");
+                    laco = false;
+                    break;
+                default:
+                    Console.WriteLine("Opção Inválida!");
+                    break;
+            }
+            Console.WriteLine("\nPressione qualquer tecla para continuar...");
+            Console.ReadKey();
+            Console.Clear();
+        }
+
+    }
+}
---
+++ Exercicio002/exercicios/ExercicioAvancado.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio002.exercicios
+{
+    class ExercicioAvancado
+    {
+        public static void NumeroAleatorio()
+        {
+            Random random = new Random();
+            int numeroAleatorio = random.Next(1, 100);
+            bool laco = true;
+            Console.Write("Tente acertar o número entre 1 a 100!\nDigite um número:");
+            int numero = int.Parse(Console.ReadLine());
+            while (laco)
+            {
+                if (numeroAleatorio > numero)
+                {
+                    Console.WriteLine("O número é maior!");
+                }
+                else if (numeroAleatorio < numero)
+                {
+                    Console.WriteLine("O número é menor!");
+                }
+                else
+                {
+                    Console.WriteLine("Parabéns! Você acertou o número!");
+                    laco = false;
+                }
+                if (laco)
+                {
+                    Console.Write("Digite outro número:");
+                    numero = int.Parse(Console.ReadLine());
+                }
+            }
+        }
+
+        public static void ManipulacaoArray()
+        {
+            int[] array = new int[10];
+            int numeroMaior = array[0];
+            int numeroMenor = array[0];
+            int numeroMedia = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write($"Digite o {i + 1}º número: ");
+                array[i] = int.Parse(Console.ReadLine());
+            }
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] > numeroMaior)
+                {
+                    numeroMaior = array[i];
+                }
+                if (array[i] < numeroMenor)
+                {
+                    numeroMenor = array[i];
+                }
+            }
+            numeroMedia = array.Sum() / array.Length;
+            //Bubble Sort 
+            for (int i = 0; i < array.Length; i++) // esse primeiro for é para percorrer o array
+            {
+                for (int j = 0; j < array.Length; j++) // esse segundo é para comparar o valor do array e alterar a posição do valor
+                {
+                    if (array[i] < array[j]) // se o a posicao do array[i] for menor que a posicao do array[j] ele troca a posicao
+                    {
+                        int temp = array[i];
+                        array[i] = array[j];
+                        array[j] = temp;
+                    }
+                }
+            }
+            Console.WriteLine($"O número maior é: {numeroMaior}");
+            Console.WriteLine($"O número menor é: {numeroMenor}");
+            Console.WriteLine($"A média dos números é: {numeroMedia}");
+            Console.WriteLine("Os números em ordem decrescente são:");
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+        }
+
+
+        public static void CalculadoraCaixa()
+        {
+            Console.Write($"Digita o valor para saque: ");
+            decimal reais = decimal.Parse(Console.ReadLine());
+            int[] notas = { 200, 100, 50, 20, 10, 5, 2, 1 };
+            for (int i = 0; i < notas.Length; i++)
+            {
+                int quantidadeNotas = (int)(reais / notas[i]);
+                reais -= quantidadeNotas * notas[i];
+                if (quantidadeNotas != 0)
+                    Console.WriteLine($"Notas de R${notas[i]}: {quantidadeNotas}");
+            }
+        }
+
+        public static void ValidadorCPF()
+        {
+            Console.WriteLine("EM BREVE");
+        }
+        public static void MiniBanco() {
+            Console.WriteLine("EM BREVE");
+        }
+    }
+}
