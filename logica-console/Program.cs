aad1bcf03b24377c58ceda19ca7d893c5eaf5f96
breno201 <breno.queiroz@websupply.com.br>
2026-02-19 11:10:13 -0300

    Add project files.

---
+++ logica-console/Program.cs
@@ -0,0 +1,93 @@
+﻿using logica_console.exercicios;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string modelo = "1 - Calculadora simples\n2 - Par ou Ímpar\n3 - Tabuada\n4 - Maior de Três\n5 - Contagem Regressiva\n6 - FizzBuzz\n7 - Verificador de palíndromo\n8 - Sequência de Fibonacci\n9 - Calculadora de média\n10 - Número primo\n0 - Encerrar";
+
+        Console.WriteLine("Exercicio 1\n");
+        Console.WriteLine("Escolha o Exercicio de sua preferencia e teste:\n");
+        bool laco = true;
+
+        while (laco)
+        {
+            Console.WriteLine(modelo);
+            Console.Write("Digite: ");
+            int escolha = int.Parse(Console.ReadLine());
+
+            switch (escolha)
+            {
+                case 1:
+                    Console.Write("Digite um numero: ");
+                    double numero1 = double.Parse(Console.ReadLine());
+                    Console.Write("Digite outro numero: ");
+                    double numero2 = double.Parse(Console.ReadLine());
+                    Console.WriteLine($"${numero1} + ${numero2} = ${ExercicioIniciante.Exercicio01(numero1, numero2)}");
+                    break;
+                case 2:
+                    Console.Write("Digite um numero para saber se é par ou ímpar: ");
+                    int numero3 = int.Parse(Console.ReadLine());
+                    Console.WriteLine(ExercicioIniciante.Exercicio02(numero3));
+                    break;
+                case 3:
+                    Console.Write("Digite um numero: ");
+                    int numero4 = int.Parse(Console.ReadLine());
+                    ExercicioIniciante.Exercicio03(numero4);
+                    break;
+                case 4:
+                    Console.Write("Digite um numero: ");
+                    int numero01 = int.Parse(Console.ReadLine());
+                    Console.Write("Digite outro numero: ");
+                    int numero02 = int.Parse(Console.ReadLine());
+                    Console.Write("Digite outro numero: ");
+                    int numero03 = int.Parse(Console.ReadLine());
+                    ExercicioIniciante.Exercicio04(numero01, numero02, numero03);
+                    break;
+                case 5:
+                    Console.Write("Digite um numero: ");
+                    int numero = int.Parse(Console.ReadLine());
+                    ExercicioIniciante.Exercicio05(numero);
+                    break;
+                case 6:
+                    ExercicioIniciante.Exercicio06();
+                    break;
+                case 7:
+                    Console.Write("Digite uma palavra: ");
+                    string palavra = Console.ReadLine();
+                    Console.WriteLine(ExercicioIniciante.Exercicio07(palavra));
+                    break;
+                case 8:
+                    Console.Write("Digite um número para Fibonacci:");
+                    int n = int.Parse(Console.ReadLine());
+                    ExercicioIniciante.Exercicio08(n);
+                    break;
+                case 9:
+                    Console.Write("Digite a quantidades de notas para calcular a nota:");
+                    int quantNota = int.Parse(Console.ReadLine());
+                    double[] notas = new double[quantNota];
+                    for(int i =0; i< notas.Length; i++)
+                    {
+                        Console.WriteLine($"Digite a {i}° nota");
+                        notas[i] = double.Parse(Console.ReadLine());
+                    }
+                    ExercicioIniciante.Exercicio09(notas);
+                    break;
+                case 10:
+                    ExercicioIniciante.Exercicio10();
+                    break;
+                case 0:
+                    Console.WriteLine("Programa Encerrado!");
+                    laco = false;
+                    break;
+                default:
+                    Console.WriteLine("Opção Inválida!");
+                    break;
+            }
+            Console.WriteLine("\nPressione qualquer tecla para continuar...");
+            Console.ReadKey();
+            Console.Clear();
+        }
+
+    }
+}
---
+++ logica-console/exercicios/ExercicioIniciante.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace logica_console.exercicios
+{
+    class ExercicioIniciante
+    {
+        public static double Exercicio01(double numero01, double numero02)
+        {
+            return numero01 + numero02;
+        }
+        public static string Exercicio02(int numero)
+        {
+            string impar = numero % 2 == 0 ? $"O número {numero} é par!" : $"O número {numero} é ímpar!";
+            return impar;
+        }
+        public static void Exercicio03(int numero)
+        {
+            for (int i = 1; i < 11; i++)
+            {
+                int tabuada = numero * i;
+                Console.WriteLine($"{numero} X {i} = {tabuada}");
+            }
+        }
+        public static void Exercicio04(int numero01, int numero02, int numero03)
+        {
+            if (numero01 > numero02 && numero02 < numero03)
+            {
+                Console.WriteLine($"Número {numero01} é o maior de todos");
+            }
+            else if (numero02 > numero01 && numero01 > numero03)
+            {
+                Console.WriteLine($"Número {numero02} é o maior de todos");
+            }
+            else
+            {
+                Console.WriteLine($"Número {numero03} é o maior de todos");
+            }
+        }
+        public static void Exercicio05(int numero)
+        {
+            for (int i = numero; i >= 0; i--)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        public static void Exercicio06()
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("FIZZBUZZ");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("FIZZ");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("BUZZ");
+                }
+                else { Console.WriteLine(i); }
+            }
+        }
+        public static string Exercicio07(string palavra)
+        {
+            string invertida = new string(palavra.Reverse().ToArray());
+            string palindromo = invertida == palavra ? "É Palíndromo!" : "Não é Palíndromo";
+            return palindromo;
+        }
+        public static void Exercicio08(int n)
+        {
+            int a = 0;
+            int b = 1;
+            Console.Write(a);
+            for (int i = 0; i < n; i++)
+            {
+                int proximo = a + b;
+                Console.Write($", {b}");
+                a = b;
+                b = proximo;
+            }
+        }
+        public static void Exercicio09(double[] notas)
+        {
+            double media = notas.Average();
+            if (media > 6.99)
+            {
+                Console.WriteLine("Aprovado!!");
+            }
+            else if (media > 4.99)
+            {
+                Console.WriteLine("Recuperação!!");
+            }
+            else
+            {
+                Console.WriteLine("Reprovado!!");
+            }
+        }
+        public static void Exercicio10()
+        {
+            List<int> numerosPrimos = new List<int>();
+            Console.Write("Digite um número: ");
+            int nPrimo = int.Parse(Console.ReadLine());
+            bool continuar = true;
+            while (continuar)
+            {
+                bool ePrimo = true;
+                if (nPrimo <= 1)
+                {
+                    ePrimo = false;
+                }
+                else
+                {
+                    for (int i = 2; i < nPrimo; i++)
+                    {
+                        if (nPrimo % i == 0)
+                        {
+                            ePrimo = false;
+                            break;
+                        }
+                    }
+                }
+                if (ePrimo)
+                {
+                    Console.WriteLine($"O número {nPrimo} é Primo");
+                    numerosPrimos.Add(nPrimo);
+                }
+                else
+                {
+                    Console.WriteLine($"O número {nPrimo} não é Primo");
+                }
+                Console.WriteLine("Deseja continuar? S/N");
+                char simOrNao = char.Parse(Console.ReadLine());
+                if (simOrNao == 'N')
+                {
+                    Console.WriteLine("Lista de números Primos citado: ");
+                    foreach (int i in numerosPrimos)
+                    {
+                        Console.WriteLine($"- {i}");
+                    }
+                    continuar = false;
+                }
+                else
+                {
+                    Console.Write("Digite outro número: ");
+                    nPrimo = int.Parse(Console.ReadLine());
+                }
+
+            }
+        }
+    }
+}
